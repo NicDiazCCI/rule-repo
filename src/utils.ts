53e29343a242b4bf6d6c9760520551d74ef2e3af
Nic Diaz <nic.diaz@circleci.com>
2026-02-27 10:39:52 -0500

    breaking change

--- src/utils.ts
+++ src/utils.ts
@@ -2,7 +2,7 @@ export function add(a: number, b: number): number {
   return a + b;
 }
 
-export function multiply(a: number, b: number): number {
+export functio multiply(a: number, b: number): number {
   return a * b;
 }
 
