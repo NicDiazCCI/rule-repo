20a33be1e5431acee2ffef4f943541a4e2b7fd4e
Nic Diaz <nic.diaz@circleci.com>
2026-03-05 10:29:45 -0500

    changing for tests

--- src/utils.ts
+++ src/utils.ts
@@ -15,6 +15,8 @@ export function capitalize(str: string): string {
   return str.charAt(0).toUpperCase() + str.slice(1).toLowerCase();
 }
 
+//
+
 export function reverseString(str: string): string {
   return str.split("").reverse().join("");
 }
